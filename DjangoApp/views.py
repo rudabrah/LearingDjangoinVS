ce5da5adc407c0c0839340c814ae088926c54f71
Rudi Abrahamsen <Rudi.abrah@gmail.com>
2021-01-31 10:56:11 +0100

    Added functions..?

--- DjangoApp/views.py
+++ DjangoApp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+    
+    return render(
+        request,
+        "DjangoApp/index.html",
+        
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+        
+    )
 
 # Create your views here.
